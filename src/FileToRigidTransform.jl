df8d64c4972eee178d4bb1c78ccff028b1af09b4
hustf <hustf@users.noreply.github.com>
2020-04-08 21:12:14 +0200

    Files generated by PkgTemplates

---
+++ src/FileToRigidTransform.jl
@@ -0,0 +1,5 @@
+module FileToRigidTransform
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using FileToRigidTransform
+using Test
+
+@testset "FileToRigidTransform.jl" begin
+    # Write your own tests here.
+end
